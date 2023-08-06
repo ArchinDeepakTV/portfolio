81dbf703629d093a6372699ce0d72f7f89c0c6a3
ADeepakTV <archindeepakad.ad@gmail.com>
2023-08-06 20:30:44 +0530

    no changes

--- projects/calculator/calc.js
+++ projects/calculator/calc.js
@@ -1,25 +1,8 @@
-function myFunction() {
-  // Get the checkbox
-  var checkBox = document.getElementById("myCheck");
-  // Get the output text
-  var text = document.getElementById("outCal");
-
-  // If the checkbox is checked, display the output text
-  if (checkBox.checked == true) {
-    text.style.display = "block";
-  } else {
-    text.style.display = "none";
-  }
-}
-
 function queryFunction() {
   var cin = document.getElementById("inQuery");
   let a = cin.value;
   let res = 0;
-
-  let part = a.slice(a.length - 1);
-  console.log(part);
-
+  let arr = new Array();
   //   console.log(typeof res);
   var text = document.getElementById("outCal");
   text.style.display = "block";
